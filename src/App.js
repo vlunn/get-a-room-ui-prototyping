b32f4f34c53bd2e0bd81f88e110a95658773db8b
Vivian Lunnikivi <vivian.lunnikivi@tuni.fi>
2021-10-05 20:38:45 +0300

    Add gradient background to settings page as well

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import SettingsPage from './components/SettingsPage';
 //import BookingPage from './components/BookingPage';
-import BookingPagev2 from './components/BookingPagev2';
+//import BookingPagev2 from './components/BookingPagev2';
 
 const App = () => {
   return (
     <div className="App">
-      {/*<SettingsPage></SettingsPage>
-      <BookingPage></BookingPage>*/}
-      <BookingPagev2></BookingPagev2>
+      <SettingsPage></SettingsPage>
+      {/*<BookingPage></BookingPage>
+      <BookingPagev2></BookingPagev2>*/}
     </div>
   )
 }
